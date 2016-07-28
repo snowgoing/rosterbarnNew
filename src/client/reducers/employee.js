06cebdc4aee242dd200b115769fcfd87b830bc08
snowgoing <snowgoing@gmail.com>
2016-07-27 19:56:44 -0700

    Initial Commit

---
+++ src/client/reducers/employee.js
@@ -0,0 +1,29 @@
+const employeeInitialState = {
+	employeeMonthlySchedule: [],
+	employeeInfo: {}
+}
+
+export default function(state = employeeInitialState, action){
+
+	switch (action.type) {
+
+		case 'GET_EMPLOYEEMONTHLYSCHEDULE':
+			return {
+				employeeMonthlySchedule: action.employeeMonthlySchedule,
+				employeeInfo: state.employeeInfo
+			}
+
+		case 'THROW_EMPLOYEEINFO':
+		// console.log(action.employeeInfo);
+			return {
+				employeeMonthlySchedule: state.employeeMonthlySchedule,
+				employeeInfo: action.employeeInfo
+
+			}
+
+	default:
+		return state;
+
+	}
+}
+
